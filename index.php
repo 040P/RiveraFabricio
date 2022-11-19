0d9fadd854a518ec29fc0217c8b4ff5cbd3e8404
040P <pfabricioriv@hotmail.com>
2022-11-19 02:56:29 -0500

    Mejoras al sitio web version Uno

--- index.php
+++ index.php
@@ -83,13 +83,13 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Fabricio"; 
+document.title="Fabricio"; 
 document.getElementById("foto").src="./images/stalin.png";
 document.getElementById("elnombre").innerHTML="Fabricio Rivera";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Jair Escobar</a></p><p><a href='eys-damaris.php'>Junior Games</a></p></p><p><a href='eys-damaris.php'>Christopher Solorzano</a></p><p><a href='eys-damaris.php'>Alexander Coral</a></p> ";
  </script>
  
 <?php
