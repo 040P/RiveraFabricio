1ad0a71455eb73a7171650089f7ddc94d488ece7
040P <pfabricioriv@hotmail.com>
2022-11-19 13:02:19 -0500

    Mejoras al sitio web version Tres

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ document.getElementById("elnombre").innerHTML="Fabricio Rivera";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='http://highkickesmeraldas.com/EscobarJair/'>Jair Escobar</a></p><p><a href='https://highkickesmeraldas.com/GamezRobert/'>Junior Games</a></p></p><p><a href='https://highkickesmeraldas.com/SolorzanoChristopher/'>Christopher Solorzano</a></p><p><a href='https://highkickesmeraldas.com/CORALalexander/'>Alexander Coral</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='http://highkickesmeraldas.com/EscobarJair/'>Jair Escobar</a></p><p><a href='https://highkickesmeraldas.com/GamezRobert/'>Junior Games</a></p></p><p><a href='https://highkickesmeraldas.com/SolorzanoChristopher/'>Christopher Solorzano</a></p><p><a href='https://highkickesmeraldas.com/CORALalexander/'>Alexander Coral</a></p><p><a href='https://chighkickesmeraldas.com/ValdezGerardo/'>Jerardo Valdez</a></p> ";
  </script>
 
 <?php
